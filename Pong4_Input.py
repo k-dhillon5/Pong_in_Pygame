c7ccc229a87f968cd4b8e432c83995ac136b41e4
k-dhillon5 <83429474+k-dhillon5@users.noreply.github.com>
2022-01-14 13:24:21 -0700

    Update Pong4_Input.py

--- Pong4_Input.py
+++ Pong4_Input.py
@@ -1,77 +1,91 @@
-import pygame, sys
-
-def ball_animation():
-	global ball_speed_x, ball_speed_y
-	
-	ball.x += ball_speed_x
-	ball.y += ball_speed_y
-
-	if ball.top <= 0 or ball.bottom >= screen_height:
-		ball_speed_y *= -1
-	if ball.left <= 0 or ball.right >= screen_width:
-		ball_speed_x *= -1
-
-	if ball.colliderect(player) or ball.colliderect(opponent):
-		ball_speed_x *= -1
-
-def player_animation():
-	player.y += player_speed
-
-	if player.top <= 0:
-		player.top = 0
-	if player.bottom >= screen_height:
-		player.bottom = screen_height
-
-# General setup
+import pygame
+import sys
+# setup
 pygame.init()
-clock = pygame.time.Clock()
 
-# Main Window
-screen_width = 1280
-screen_height = 960
-screen = pygame.display.set_mode((screen_width,screen_height))
+clock = pygame.time.Clock()
+Black = (0, 0, 0)
+White = (255, 255, 255)
+Teal = (12, 201, 157)
+Purple = (153, 51, 255)
+Grey = (200, 200, 200)
+
+screen_width = 500
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
-# Colors
-light_grey = (200,200,200)
-bg_color = pygame.Color('grey12')
 
-# Game Rectangles
-ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
-player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10,140)
-opponent = pygame.Rect(10, screen_height / 2 - 70, 10,140)
+def move_ball():
+    global ball_speed_x, ball_speed_y
 
-# Game Variables
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+
+def move_player():
+    player.y += player_speed
+
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+
+def auto_opp():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+
+# platforms
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_width/2 - 30, 10, 140)
+opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+
 ball_speed_x = 7
 ball_speed_y = 7
 player_speed = 0
+opponent_speed = 7
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP:
-				player_speed -= 7
-			if event.key == pygame.K_DOWN:
-				player_speed += 7
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_UP:
-				player_speed += 7
-			if event.key == pygame.K_DOWN:
-				player_speed -= 7
-	
-	# Game Logic
-	ball_animation()
-	player_animation()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
+    move_ball()
+    move_player()
+    auto_opp()
+    # visuals
+    screen.fill(White)
+    pygame.draw.rect(screen, Teal, player)
+    pygame.draw.rect(screen, Purple, opponent)
+    pygame.draw.ellipse(screen, Grey, ball)
+    pygame.draw.aaline(screen, Black, (screen_width/2, 0),
+                       (screen_width/2, screen_height))
 
-	# Visuals
-	screen.fill(bg_color)
-	pygame.draw.rect(screen, light_grey, player)
-	pygame.draw.rect(screen, light_grey, opponent)
-	pygame.draw.ellipse(screen, light_grey, ball)
-	pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
-
-	pygame.display.flip()
-	clock.tick(60)
+    pygame.display.flip()
+    clock.tick(60)
